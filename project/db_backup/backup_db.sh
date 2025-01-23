b18f8d8d5f5ea1f8a0fc53037136b448a554e53c
ArttuLe <lehtonen.arttu@yahoo.fi>
2025-01-23 10:34:10 +0200

    fix: backupdb.sh and index html route stuff

--- project/db_backup/backup_db.sh
+++ project/db_backup/backup_db.sh
@@ -2,7 +2,12 @@
 
 set -e
 
-
+apt-get update && apt-get install -y \
+    software-properties-common \
+    wget \
+    gnupg2 \
+    lsb-release
+    
 sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
 apt-get update
