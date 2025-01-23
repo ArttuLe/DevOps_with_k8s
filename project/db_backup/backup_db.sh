345b351524844bb775ef8ee45e890a70713f8c7d
ArttuLe <lehtonen.arttu@yahoo.fi>
2025-01-23 10:22:13 +0200

    fix: remove sudos from script

--- project/db_backup/backup_db.sh
+++ project/db_backup/backup_db.sh
@@ -4,7 +4,7 @@ set -e
 
 
 sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
 apt-get update
 apt-get install -y postgresql-client-16
 
