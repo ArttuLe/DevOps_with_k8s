4521207da669b50019facf298880546b6694a5c6
ArttuLe <lehtonen.arttu@yahoo.fi>
2025-01-23 10:21:32 +0200

    fix: backup db script

--- project/db_backup/backup_db.sh
+++ project/db_backup/backup_db.sh
@@ -3,10 +3,10 @@
 set -e
 
 
-sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
-sudo apt-get update
-sudo apt-get install -y postgresql-client-16
+apt-get update
+apt-get install -y postgresql-client-16
 
 if [ $URL ]; then
   pg_dump -v $URL > /usr/src/app/backup.sql
