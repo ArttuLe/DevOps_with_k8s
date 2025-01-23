ad6c5a38c5054d80c398707b901916ceb13896a7
ArttuLe <lehtonen.arttu@yahoo.fi>
2025-01-23 09:38:09 +0200

    fix: commit the modified backup.sh

--- project/db_backup/backup_db.sh
+++ project/db_backup/backup_db.sh
@@ -2,7 +2,13 @@
 
 set -e
 
-if [ $URL ]
-then
+if [ $URL ]; then
   pg_dump -v $URL > /usr/src/app/backup.sql
+  
+  gsutil cp /usr/src/app/backup.sql gs://dwk-gke/backup-$(date +%Y-%m-%d_%H-%M-%S).sql
+  
+  echo "Backup completed and uploaded to Google Cloud Storage."
+else
+  echo "Database URL not provided. Set the URL environment variable."
+  exit 1
 fi
